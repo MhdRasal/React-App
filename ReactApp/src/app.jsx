1efd622adba347f9753f6f92215be4f830053e7a
Muhammed Rasal <mrasalofficial@gmail.com>
2025-09-28 00:16:11 +0530

    counter creation

--- ReactApp/src/app.jsx
+++ ReactApp/src/app.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
+import Counter from './components/counter.jsx';
 import './app.css';
 
+
 function App(props){
+    const [count, setCount] =useState(0)
+    const addCount = () => {
+        setCount(count + 1)
+        console.log(count);
+    }
+    let obj ={
+        title: "1st counter",
+        count
+    }
     return (
         <div>
             <h1 style={{color: 'red'}}>Hello World!</h1>
-            <p>Hi, {props.name}</p>
+            <Counter {...obj}/>
+            <button onClick={addCount}>Click Me</button>
         </div>
-
     )
 }
 export default App
---
+++ ReactApp/src/components/counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function counter(obj) {
+  return (
+    <div>
+        <p>Counter{obj.title} : {obj.count}</p>
+        <p>Counter{obj.title} : {obj.count}</p>
+
+    </div>
+  )
+}
