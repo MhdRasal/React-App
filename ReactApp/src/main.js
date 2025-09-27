125223d580c031cbd02bddcc9ed05e7517115f4b
Muhammed Rasal <mrasalofficial@gmail.com>
2025-09-27 10:13:20 +0530

    2nd day

--- ReactApp/src/main.js
+++ ReactApp/src/main.js
@@ -1,3 +1,5 @@
-import React from 'react'
-import { setupCounter } from './app.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app.js';
 
+ReactDOM.createRoot('#app').innerHTML = ReactDOMServer.renderToString(<App />);
